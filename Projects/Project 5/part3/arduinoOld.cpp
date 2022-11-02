c7c05697139a21f6697e558a3ee5c67c0e89c6f8
Gunnlaug18 <69977134+Gunnlaug18@users.noreply.github.com>
2022-11-02 19:24:00 +0000

    Update arduinoOld.cpp

--- Projects/Project 5/part3/arduinoOld.cpp
+++ Projects/Project 5/part3/arduinoOld.cpp
@@ -1,9 +1,8 @@
 #include <Arduino.h>
-#include "digital_out.h"
-#include <digital_in_b.h>
-#include <digital_in.h>
-#include <encoder.h>
-#include <timer0_ms.h>
+// #include "digital_out.h"
+// #include <digital_in.h>
+// #include <encoder.h>
+// #include <timer0_ms.h>
 
 #define SET_OP 0x01
 #define STOP_NODE 0x02
@@ -11,34 +10,41 @@
 #define RESET_NODE 0x81
 #define RESET_COMMS 0x82
 
-Digital_in encoder_c1(DDD2);
-Digital_in encoder_c2(DDD3);
-Digital_out motor_m1(DDD4);
-Digital_out motor_m2(DDD5);
-Digital_out pwmPIN(DDD6);
-Encoder encoder(10);
+// Digital_in encoder_c1(DDD2);
+// Digital_in encoder_c2(DDD3);
+// Digital_out motor_m1(DDD4);
+// Digital_out motor_m2(DDD5);
+// Digital_out pwmPIN(DDD6);
+// Encoder encoder(10);
 // PI_controller controller(1.0, 0.064);
-Timer0_ms timer0;
-pwmPIN.init();
-timer0.init();
-encoder.timer_msec(10);
-encoder_c1.init();
-encoder_c2.init();
-motor_m1.init();
-motor_m2.init();
-
-volatile int posi = 0;
+// Timer0_ms timer0;
+// pwmPIN.init();
+// timer0.init();
+// encoder.timer_msec(10);
+// encoder_c1.init();
+// encoder_c2.init();
+// motor_m1.init();
+// motor_m2.init();
 
 int ledPin = 11;          // LED with PWM brightness control
+int PWM = 6; //PWM PIN
+int Motor1 = 4;
+int Motor2 = 5;
 uint8_t server_num = 0;
 uint16_t registers[100];
 int ThermistorPin = A0;
 uint8_t lastState = 0;
+
+
 void setup() {            // called once on start up
   // A baud rate of 115200 (8-bit with No parity and 1 stop bit)
   Serial.begin(115200, SERIAL_8N1);
   pinMode(ledPin, OUTPUT);         // the LED is an output
   pinMode(ThermistorPin,INPUT);
+  pinMode(Motor1,OUTPUT);
+  pinMode(Motor2,OUTPUT);
+  pinMode(PWM,OUTPUT);
+
 }
 
 
@@ -67,20 +73,27 @@ void statemachine(uint16_t state, uint16_t regist){
     case SET_PREOP:
       if (lastState != SET_PREOP)
       {
-       lastState = SET_PREOP
+        digitalWrite(Motor1,LOW);
+        digitalWrite(Motor2,HIGH);
+        lastState = SET_PREOP;
       }
       break;
     case SET_OP:
       if (lastState != SET_OP)
       {
-        TIMSK1 &= ~(1<<OCIE1A);
+        // TIMSK1 &= ~(1<<OCIE1A);
+        digitalWrite(Motor1,LOW);
+        digitalWrite(Motor2,HIGH);
+        analogWrite(PWM,(registers[regist])/4);
         //controller.go();
-        lastState = SET_OP
+        lastState = SET_OP;
       }
       break;
     case STOP_NODE:
       if (lastState != STOP_NODE)
       {
+        digitalWrite(Motor1,LOW);
+        digitalWrite(Motor2,LOW);
         //controller.brake();
       }
       break;
@@ -113,29 +126,36 @@ void loop() {
             buffer[1] = command[1];
             buffer[2] = command[2];
             buffer[3] = command[3];
-            if (regis == 0){
-              //statemachine(data,regis);
-              uint16_t signal = analogRead(ThermistorPin);
-              lastState = data;
-              uint8_t data1 = signal>>8;
-              uint8_t data2 = signal;
-              buffer[4] = data1;
-              buffer[5] = data2;
-              registers[regis] = signal;
-            }else{
-              uint8_t data1 = registers[regis]>>8;
-              uint8_t data2 = registers[regis];
-              buffer[4] = data1;
-              buffer[5] = data2;
-            }
+            uint16_t signal = analogRead(ThermistorPin);
+            uint8_t data1 = signal>>8;
+            uint8_t data2 = signal;
+            buffer[4] = data1;
+            buffer[5] = data2;
+            lastState = data;
+            registers[regis] = signal;
+            // if (regis == 0){
+            //   //statemachine(data,regis);
+            // }else{
+            //   uint8_t data1 = registers[regis]>>8;
+            //   uint8_t data2 = registers[regis];
+            //   buffer[4] = data1;
+            //   buffer[5] = data2;
+            // }
             uint16_t crc_update = ModRTU_CRC(buffer,6);
             buffer[6] = (crc_update >>8);
             buffer[7] = crc_update;
             Serial.write(buffer,8);               // send the buffer to the RPi
         }//Write
         else if (funct == 6){
-            statemachine(data,regis);
+          if (regis ==0){
+            statemachine(data,6);
+          }else if(regis == 6){
+            registers[regis] = data;
+          }else{
+            registers[regis] = data;
+          }
             lastState = data;
+            buffer[0] = command[0];
             buffer[1] = command[1];
             buffer[2] = command[2];
             buffer[3] = command[3];
@@ -143,7 +163,6 @@ void loop() {
             buffer[5] = command[5];
             buffer[6] = command[6];
             buffer[7] = command[7];
-            registers[regis] = data;
             Serial.write(buffer,8);               // send the buffer to the RPi
         }
       }else{
