615af87ce10f28e7a29fd75ea440820450566e52
Gunnlaug18 <69977134+Gunnlaug18@users.noreply.github.com>
2022-11-03 17:09:39 +0000

    Update arduinoOld.cpp

--- Projects/Project 5/part3/arduinoOld.cpp
+++ Projects/Project 5/part3/arduinoOld.cpp
@@ -1,8 +1,4 @@
 #include <Arduino.h>
-// #include "digital_out.h"
-// #include <digital_in.h>
-// #include <encoder.h>
-// #include <timer0_ms.h>
 
 #define SET_OP 0x01
 #define STOP_NODE 0x02
@@ -10,21 +6,7 @@
 #define RESET_NODE 0x81
 #define RESET_COMMS 0x82
 
-// Digital_in encoder_c1(DDD2);
-// Digital_in encoder_c2(DDD3);
-// Digital_out motor_m1(DDD4);
-// Digital_out motor_m2(DDD5);
-// Digital_out pwmPIN(DDD6);
-// Encoder encoder(10);
-// PI_controller controller(1.0, 0.064);
-// Timer0_ms timer0;
-// pwmPIN.init();
-// timer0.init();
-// encoder.timer_msec(10);
-// encoder_c1.init();
-// encoder_c2.init();
-// motor_m1.init();
-// motor_m2.init();
+
 
 int ledPin = 11;          // LED with PWM brightness control
 int PWM = 6; //PWM PIN
@@ -46,6 +28,56 @@ void setup() {            // called once on start up
   pinMode(PWM,OUTPUT);
 
 }
+
+char buff[5];
+float error = 0.0;
+float the_pwm = 0.0;
+float scaler = 100000;
+float sum = 0.0;
+float value = 0.0;
+float integral = 0.0;
+
+
+//PI controller values
+float K_p =  0.001;
+float t_i = 2.7;
+float t = 0.9;
+
+
+int update(float ref, float actual) {
+    
+    error = ref - actual*10;
+    value = sum + (error * t);
+    if(actual > 1 ){
+        sum = value;
+    }
+    integral = (sum/t_i);
+    //PWM range from 00000 up to 99999 (max is 100000), because period set at 100000.
+    the_pwm = (K_p * (error + integral)) * scaler;
+
+    if(the_pwm > scaler-1)
+    {
+        the_pwm = scaler-1;
+    }
+    if(the_pwm <1)
+    {
+        the_pwm = 0;
+    }
+    printf("Required speed in PPS: %f\n", ref);
+    printf("Speed in PPS: %f\n", actual*10);  
+
+    return (int)the_pwm;
+}
+
+
+void set_speed(int pwm_val)
+{
+    fd = open("/sys/class/pwm/pwmchip0/pwm0/duty_cycle", O_WRONLY);
+    sprintf(buff, "%d", pwm_val);
+    write(fd, buff, sizeof(buff));
+    close(fd);
+}
+
 
 
 // Compute the MODBUS RTU CRC
@@ -73,18 +105,18 @@ void statemachine(uint16_t state, uint16_t regist){
     case SET_PREOP:
       if (lastState != SET_PREOP)
       {
+        digitalWrite(ledPin,HIGH);
         digitalWrite(Motor1,LOW);
         digitalWrite(Motor2,HIGH);
         lastState = SET_PREOP;
       }
+
       break;
     case SET_OP:
       if (lastState != SET_OP)
       {
         // TIMSK1 &= ~(1<<OCIE1A);
-        digitalWrite(Motor1,LOW);
-        digitalWrite(Motor2,HIGH);
-        analogWrite(PWM,(registers[regist])/4);
+        analogWrite(PWM,map(registers[regist],0,1023,0,255));
         //controller.go();
         lastState = SET_OP;
       }
@@ -117,6 +149,8 @@ void loop() {
     uint16_t crc = ((command[6] << 8) + command[7]);
     
     uint16_t crc_comapre = ModRTU_CRC(command,6);
+
+    //Serial.write(command,8);
 
     if (crc == crc_comapre){
       if (server_num == server){
